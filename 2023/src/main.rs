987bdf8dd28016696775643ef68e6bcb1bb13761
Rudi Blaha Svartveit <rudsvar@gmail.com>
2023-12-22 23:53:23 +0100

    Fix last bug

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,56 +1,45 @@
-use std::collections::HashMap;
-
-fn first_digit(s: &str) -> Option<char> {
-    s.chars().find(|c| c.is_ascii_digit())
-}
-
-fn last_digit(s: &str) -> Option<char> {
-    s.chars().rev().find(|c| c.is_ascii_digit())
+fn name_to_digit(name: &str) -> Option<u32> {
+    match name {
+        "0" => Some(0),
+        "1" | "one" => Some(1),
+        "2" | "two" => Some(2),
+        "3" | "three" => Some(3),
+        "4" | "four" => Some(4),
+        "5" | "five" => Some(5),
+        "6" | "six" => Some(6),
+        "7" | "seven" => Some(7),
+        "8" | "eight" => Some(8),
+        "9" | "nine" => Some(9),
+        _ => None,
+    }
 }
 
 fn first_and_last_digit(s: &str) -> Option<u32> {
-    let s = replace_names_with_digit(s);
-    let first = first_digit(&s)?;
-    let last = last_digit(&s)?;
+    let digits = [
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine",
+    ];
+    let first = name_to_digit(find_first_element(s, &digits)?)?;
+    let last = name_to_digit(find_last_element(s, &digits)?)?;
     format!("{}{}", first, last).parse().ok()
 }
 
-fn find_earliest_element<'a>(s: &str, words: &[&'a str]) -> Option<(usize, &'a str)> {
+fn find_first_element<'a>(s: &str, words: &[&'a str]) -> Option<&'a str> {
     words
         .iter()
         .map(|word| (s.find(word), word))
         .filter_map(|(pos, word)| pos.map(|pos| (pos, *word)))
         .min_by_key(|(pos, _)| *pos)
+        .map(|(_, word)| word)
 }
 
-fn replace_names_with_digit(s: &str) -> String {
-    let words = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let word_to_idx: HashMap<&str, usize> = words
+fn find_last_element<'a>(s: &str, words: &[&'a str]) -> Option<&'a str> {
+    words
         .iter()
-        .enumerate()
-        .map(|(i, word)| (*word, i + 1))
-        .collect();
-
-    let mut s = s.to_string();
-    if let Some((pos, word)) = find_earliest_element(&s, &words) {
-        let idx = word_to_idx[word];
-        s = s.replace(&s[pos..pos + word.len()], &idx.to_string())
-    }
-
-    s = s.chars().rev().collect::<String>();
-    let words_rev = words.map(|word| word.chars().rev().collect::<String>());
-    let words_rev: Vec<&str> = words_rev.iter().map(|w| w.as_ref()).collect();
-
-    if let Some((pos, word)) = find_earliest_element(&s, &words_rev) {
-        let word = word.chars().rev().collect::<String>();
-        let idx = word_to_idx[word.as_str()];
-        s = s.replace(&s[pos..pos + word.len()], &idx.to_string())
-    }
-    s = s.chars().rev().collect::<String>();
-
-    s
+        .map(|word| (s.rfind(word), word))
+        .filter_map(|(pos, word)| pos.map(|pos| (pos, *word)))
+        .max_by_key(|(pos, _)| *pos)
+        .map(|(_, word)| word)
 }
 
 fn main() {
@@ -70,40 +59,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_first_digit() {
-        assert_eq!(first_digit("abc123"), Some('1'));
-        assert_eq!(first_digit("abc"), None);
-    }
-
-    #[test]
-    fn test_last_digit() {
-        assert_eq!(last_digit("abc123"), Some('3'));
-        assert_eq!(last_digit("abc"), None);
-    }
-
-    #[test]
     fn test_first_and_last_digit() {
         assert_eq!(first_and_last_digit("abc123"), Some(13));
         assert_eq!(first_and_last_digit("abc"), None);
-    }
-
-    #[test]
-    fn test_replace_names_with_digit() {
-        assert_eq!(
-            replace_names_with_digit("one two three four five six seven eight nine"),
-            "1 two three four five six seven eight 9"
-        );
-    }
-
-    #[test]
-    fn test_replace_names_with_digit_2() {
-        assert_eq!(replace_names_with_digit("two1nine"), "219");
-        assert_eq!(replace_names_with_digit("eightwothree"), "8wo3");
-        assert_eq!(replace_names_with_digit("abcone2threexyz"), "abc123xyz");
-        assert_eq!(replace_names_with_digit("xtwone3four"), "x2ne34");
-        assert_eq!(replace_names_with_digit("4nineeightseven2"), "49eight72");
-        assert_eq!(replace_names_with_digit("zoneight234"), "z1ight234");
-        assert_eq!(replace_names_with_digit("7pqrstsixteen"), "7pqrst6teen");
     }
 
     #[test]
@@ -115,5 +73,19 @@ mod tests {
         assert_eq!(first_and_last_digit("4nineeightseven2"), Some(42));
         assert_eq!(first_and_last_digit("zoneight234"), Some(14));
         assert_eq!(first_and_last_digit("7pqrstsixteen"), Some(76));
+    }
+
+    #[test]
+    fn test_find_first_of() {
+        assert_eq!(find_first_element("abc123", &["abc"]), Some("abc"));
+        assert_eq!(find_first_element("abc123", &["123", "b"]), Some("b"));
+        assert_eq!(find_first_element("abc123", &["123", "abc"]), Some("abc"));
+    }
+
+    #[test]
+    fn test_find_last_of() {
+        assert_eq!(find_last_element("abc123", &["abc"]), Some("abc"));
+        assert_eq!(find_last_element("abc123", &["123", "b"]), Some("123"));
+        assert_eq!(find_last_element("abc123", &["123", "abc"]), Some("123"));
     }
 }
